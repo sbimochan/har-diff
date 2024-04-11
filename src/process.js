a5c3ae601c89ad689508c744ba1b4723b0c98911
Bimochan Shrestha <bmochan@gmail.com>
2024-04-12 00:03:50 +0545

    JSON sort

--- src/process.js
+++ src/process.js
@@ -3,12 +3,22 @@ import { exec } from 'child_process';
 export function processDiff(file, folder, url) {
   const mockGenerator = `npx har-to-mocks ./${file} ./${folder} --url=${url}`;
 
-  exec(mockGenerator, (error, stdout, stderr) => {
+  exec(mockGenerator, (error, stdout) => {
     if (error) {
       console.error(`Error executing npm script: ${error.message}`);
       process.exit(1);
     }
-    console.log(stderr);
+    // console.log(stderr);
     console.log(stdout);
-  });
+  })
+  console.log('Mocks generated')
+  console.log('Sorting all json files')
+  exec(`npx json-sort-cli source*`, (error) => {
+    console.error(error)
+  })
+  exec(`npx json-sort-cli target*`, (error) => {
+    console.error(error)
+  })
+  console.log('Sorting files completed')
+
 }
